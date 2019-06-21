ac1635eb89f5cc419a9c283c2402b3987654abc3
sammorrisdesign <mail@sammorrisdesign.co.uk>
2019-06-21 15:30:14 -0400

    Start on dividers and animate them

--- src/js/modules/animation.js
+++ src/js/modules/animation.js
@@ -13,6 +13,7 @@ export default {
             render: function(data) {
                 scrollTop = data.curTop;
                 this.headerCutting();
+                this.dividerCutting();
             }.bind(this)
         });
     },
@@ -24,5 +25,13 @@ export default {
             var frame = Math.floor(scrollTop / 50 % 3) + 1;
             $man.attr('class', 'uit-header__man show-frame-' + frame);
         }
+    },
+
+    dividerCutting: function() {
+        $('.uit-body__divider').each(function() {
+            var frame = Math.floor(scrollTop / 50 % 3) + 1;
+            console.log($(this));
+            $(this).removeClass('show-frame-1 show-frame-2 show-frame-3').addClass('show-frame-' + frame)
+        });
     }
 };
