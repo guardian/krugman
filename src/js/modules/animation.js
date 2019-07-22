ee480a3af726671771fb9326cb7dc41330ea0df9
sammorrisdesign <mail@sammorrisdesign.co.uk>
2019-07-22 17:12:38 -0400

    transition the graph illustration

--- src/js/modules/animation.js
+++ src/js/modules/animation.js
@@ -10,6 +10,8 @@ export default {
             scrollTop = $(window).scrollTop();
             this.headerCutting();
             this.dividerCutting();
+            this.armTransition();
+            this.graphTransition();
             this.illustrationFrames();
         }.bind(this));
     },
@@ -28,6 +30,43 @@ export default {
             if (elHeight > scrollTop) {
                 var interimValue = (((elHeight - scrollTop) / elHeight) * 80) + 120;
                 $parent.attr('style', `background-position-y: ${interimValue}%`)
+            }
+        }
+    },
+
+    armTransition: function() {
+        $('.is-transformable').each(function(i, el) {
+            var $parent = $(el).parent();
+            var start = $parent.offset().top - $(window).height();
+            var end = $parent.offset().top - ($(window).height() / 2)
+
+            if (scrollTop > start && scrollTop < end) {
+                var startValues = $(el).data('start').split(',');
+                var endValues = $(el).data('end').split(',');
+                var unit = $(el).data('unit') || 'px';
+                var percentage = (end - scrollTop) / ($(window).height() + $parent.outerHeight()) * 100;
+
+                var xDifferencePoint = (startValues[0] - endValues[0]) / 100;
+                var yDifferencePoint = (startValues[1] - endValues[1]) / 100;
+
+                $(el).css("transform", `translate(${xDifferencePoint * percentage}${unit}, ${yDifferencePoint * percentage}${unit})`)
+            }
+        });
+    },
+
+    graphTransition: function() {
+        var $parent = $('.has-graph');
+        var start = $parent.offset().top - $(window).height();
+        var end = $parent.offset().top;
+
+        if (scrollTop > start && scrollTop < end) {
+            var percentage = (end - scrollTop) / ($(window).height() + $parent.outerHeight()) * 100;
+
+
+            if (percentage > 50) {
+                $('.stroke--first').css('stroke-dashoffset', ((percentage - 50) * -2) + '%');
+            } else {
+                $('.stroke--second').css('stroke-dashoffset', (percentage * -2) + '%');
             }
         }
     },
